f71859e871cbb7c781ec7391afb249d8cad3bfcd
Christina lytle <christina.lytle17@gmail.com>
2021-01-16 15:36:26 -0700

    final commit

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 	private static final String URL = "jdbc:mysql://localhost:3306/board_game_database"; 
 	private static final String USER_NAME = "root"; 
-	private static final String PASSWORD = "TomKipVal8?"; 
+	private static final String PASSWORD = ""; 
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER_NAME, PASSWORD); 
